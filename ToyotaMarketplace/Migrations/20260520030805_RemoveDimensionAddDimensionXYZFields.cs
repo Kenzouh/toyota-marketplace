187f58f68e02a73004810bdd16c42917d774925f
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2026-05-21 15:39:25 +0800

    feat(frontend): add car SVG and add vehicle page
    chore(db): add dimension x, y, z columns in VehicleDimensionFuels table

---
+++ ToyotaMarketplace/Migrations/20260520030805_RemoveDimensionAddDimensionXYZFields.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToyotaMarketplace.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDimensionAddDimensionXYZFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dimension",
+                table: "VehicleDimensionFuels");
+
+            migrationBuilder.AddColumn<float>(
+                name: "DimensionX",
+                table: "VehicleDimensionFuels",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "DimensionY",
+                table: "VehicleDimensionFuels",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "DimensionZ",
+                table: "VehicleDimensionFuels",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DimensionX",
+                table: "VehicleDimensionFuels");
+
+            migrationBuilder.DropColumn(
+                name: "DimensionY",
+                table: "VehicleDimensionFuels");
+
+            migrationBuilder.DropColumn(
+                name: "DimensionZ",
+                table: "VehicleDimensionFuels");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dimension",
+                table: "VehicleDimensionFuels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
